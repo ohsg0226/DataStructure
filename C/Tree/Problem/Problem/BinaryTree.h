a7daeed9acb7c9c081593b8ae5468e2452036b7f
ohsg0226 <ohsg0226@naver.com>
2022-08-08 14:34:40 +0900

    Problem 08-1 코드 추가

---
+++ C/Tree/Problem/Problem/BinaryTree.h
@@ -0,0 +1,38 @@
+//
+//  BinaryTree.h
+//  Problem
+//
+//  Created by 오승기 on 2022/08/08.
+//
+
+#ifndef BinaryTree_h
+#define BinaryTree_h
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+  BTData data;
+  struct _bTreeNode * left;
+  struct _bTreeNode * right;
+} BTreeNode;
+
+BTreeNode * MakeBTreeNode(void);
+BTData GetData(BTreeNode * bt);
+void SetData(BTreeNode * bt, BTData data);
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt);
+BTreeNode * GetRightSubTree(BTreeNode * bt);
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
+void DeleteTree(BTreeNode * bt);
+
+#endif /* BinaryTree_h */
